a31ae924e12f5e87e08363f71806e191948e6b18
Lenique Noralez <noralezl@wwt.com>
2023-03-29 15:33:25 -0500

    node implementation

---
+++ node/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import {
+  toAsciiCharacterArr,
+  toBrightnessArr,
+  openImage,
+  getImageDataBuffer,
+  chunkArray,
+  getImageMetadata,
+  bufferToArray,
+} from './utils.js';
+
+(async () => {
+  const image = openImage('output.jpg');
+
+  const { width } = await getImageMetadata(image);
+  const { buffer } = await getImageDataBuffer(image);
+  const pixelArray = bufferToArray(buffer);
+  const chunked = chunkArray(pixelArray, width);
+  const brightnessArr = chunked.map((chunk) => toBrightnessArr(chunk));
+  const asciiArr = brightnessArr.map((arr) =>
+    toAsciiCharacterArr(arr).join('')
+  );
+
+  console.log(asciiArr.join('\n'));
+})();
